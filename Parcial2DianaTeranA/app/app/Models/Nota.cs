e34ea5014dfb98524f0589a5f3c1d9b1bc6aae9e
dianateranargandona@gmail.com <dianateranargandona@gmail.com>
2019-05-25 10:42:08 -0400

    Commit mvvm

---
+++ Parcial2DianaTeranA/app/app/Models/Nota.cs
@@ -0,0 +1,14 @@
+﻿namespace app.Models
+{
+    class Nota
+    {
+        [Key]
+        public int NotaId { get; set; }
+
+        [Required]
+        [Display(Nota = "Nota")]
+        public string Nota { get; set; }
+
+     
+    }
+}
---
+++ Parcial2DianaTeranA/app/app/ViewModel/NoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app.ViewModel
+
+{
+    class NoteViewModel
+    {
+        #region variables
+        String nota;
+
+        #endregion
+
+        #region propiedades
+        public string Nota { get; set; }
+
+    }
+}
